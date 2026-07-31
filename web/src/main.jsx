482f34d63dfe9e51e12c73729e8d39641f760d89
niteflights <33931228+niteflights@users.noreply.github.com>
2026-07-31 23:47:24 +0300

    Update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import 'maplibre-gl/dist/maplibre-gl.css'
+import 'leaflet/dist/leaflet.css'
 import './styles.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
